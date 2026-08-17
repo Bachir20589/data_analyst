40214685bbb6f25af481a1139443654a990f0357
Bachir1905 <ndiaye.bassirou17@ugb.edu.sn>
2026-08-17 16:00:52 +0000

    second

---
+++ jour4.py
@@ -0,0 +1,18 @@
+print(range(6))
+
+for i in  range(6):
+    print(i)
+
+def first():
+    for i in  range(6):
+        print(i)
+
+first()
+
+
+def somme(a,b):
+
+    s=a+b
+    print(s)
+
+somme (4,6)
